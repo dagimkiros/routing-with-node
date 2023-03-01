cb38ec8a0d291f89a564f7ac4214c6686d84fb15
Dagm Kiros <>
2023-03-01 05:36:38 +0000

    edited the about file

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var http = require("http");
+
 http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
+  // Use the toLowerCase() method instead of toLowercase()
+  var path = req.url.toLowerCase();
+
+  switch(path) {
+    // Add a colon after the case "/"
+    case "/":
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end("Home Page");
+      break;
+
+    // Remove the semicolon after the case "/about"
+    case "/about":
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About Page');
+      break;
+
+    // Remove the colon after the default keyword
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not Found');
+  }
+  
 }).listen(process.env.PORT || 3000);
+
